92f191aaa2a95ddd04869fa5be6928380958b1ae
Sahil Gevariya <64141966+sahilgevariya@users.noreply.github.com>
2023-06-15 22:28:03 +0530

    changed easy logic

--- LLD/Sudoku_Solver.py
+++ LLD/Sudoku_Solver.py
@@ -33,15 +33,15 @@ def solveSudoku(self, board: List[List[str]]) -> None:
 
   # check if given number can take place that empty place                        
   def isValid(row,col,n):
-      topLeftRow = 3*(row//3)
-      topLeftCol = 3*(col//3)
+      topLeftRow = row - row%3  # 3*(row//3)
+      topLeftCol = col - col%3  # 3*(col//3)
 
       for i in range(9):
           # check in that row and col
           if board[row][i] == n or board[i][col] == n:
               return False
 
-          # check in that sub-board
+          # check in that sub-board (l -> r, t -> b)
           if board[ topLeftRow + i%3 ][ topLeftCol + i//3 ] == n:
               return False
       return True
